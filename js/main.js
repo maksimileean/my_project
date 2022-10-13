578d2aa7108e2d06d18cc134343a0b8bdf320b8e
maksimileean <114295776+maksimileean@users.noreply.github.com>
2022-10-13 22:13:00 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,3 +7,48 @@ const lengthCheck = (String, Length) => {
 };
 
 lengthCheck('Тестовая строка', 50);
+
+//------------------------------------------------------------------------------------------------------------------------------------
+
+const getRandomPositiveInteger = (a, b) => {
+  if (a < 0 || b < 0) {
+    return NaN;
+  }
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+function shuffle(array) {
+  let currentIndex = array.length, randomIndex;
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+const Id_numbers = [];
+const testfunc = (massive) => {
+  for (let i = 0; i < 25; i++){
+    massive[i] = i + 1;
+  }
+  shuffle(massive);
+  return massive;
+};
+
+
+const photo = (i) => ({
+  id: i,
+  url: `photos${i}.jpg`,
+  description: 'Пример описания',
+  likes: getRandomPositiveInteger(15, 200),
+  comments: getRandomPositiveInteger(0, 200)
+});
+
+const list_of_photos = Array.from({length: 25}, (a, i) => photo(i));
+console.log(list_of_photos);
