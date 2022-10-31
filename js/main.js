9751b6d0bd85b4c133a2dd12617a48277ec1d0d0
maksimileean <114295776+maksimileean@users.noreply.github.com>
2022-10-31 18:15:14 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import {listOfPhotos} from './photo-creation.js';
+// eslint-disable-next-line no-unused-vars
 import {picturesList} from './create-pictures.js';
