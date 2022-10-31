e07f0b04f66d091f3c3dcf14246492ba3464a1b8
maksimileean <114295776+maksimileean@users.noreply.github.com>
2022-10-31 18:12:35 +0300

    Update photo-creation.js

--- js/photo-creation.js
+++ js/photo-creation.js
@@ -8,6 +8,6 @@ const photo = (i) => ({
   comments: getRandomPositiveInteger(0, 200)
 });
 
-const list_of_photos = Array.from({length: 25}, (a, i) => photo(i));
+const listOfPhotos = Array.from({length: 25}, (a, i) => photo(i));
 
-export {list_of_photos};
+export {listOfPhotos};
